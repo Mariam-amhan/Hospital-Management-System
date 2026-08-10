8aa484e3babf257edeb54f05f4d3c4a552cb39d6
Mariam Amhan <amhann67@gmail.com>
2026-08-10 12:34:05 +0300

    Format Java source code
    Improve code formatting and consistency.

--- src/models/Person.java
+++ src/models/Person.java
@@ -14,42 +14,43 @@ public class Person {
         this.gender = gender;
     }
 
-     public void setId(int id) {
+    public void setId(int id) {
         this.id = id;
-     }
-     public int getId() {
+    }
+
+    public int getId() {
         return id;
-     }
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
-     public void setName(String name) {
-         this.name = name;
-     }
     public String getName() {
-         return name;
-     }
+        return name;
+    }
 
-
-     public void setAge(int age) {
+    public void setAge(int age) {
         this.age = age;
     }
+
     public int getAge() {
         return age;
     }
 
-    
     public void setGender(String gender) {
         this.gender = gender;
     }
+
     public String getGender() {
         return gender;
     }
 
-  public void displayInfo() {
-    System.out.println("ID: " + id);
-    System.out.println("Name: " + name);
-    System.out.println("Age: " + age);
-    System.out.println("Gender: " + gender);
+    public void displayInfo() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Gender: " + gender);
 
     }
 

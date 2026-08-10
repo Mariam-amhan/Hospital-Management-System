c1ae9e957526468e5d1fe8b45b885872bfdd489b
Mariam Amhan <amhann67@gmail.com>
2026-08-10 12:26:24 +0300

    Format Java source code
    Improve code formatting and consistency.

--- src/models/Appointment.java
+++ src/models/Appointment.java
@@ -1,64 +1,67 @@
 package models;
 
-
 public class Appointment {
-  private int appointmentId;
-  private Patient patient;
-  private Doctor doctor;
-  private String date;
-  private String time;
-    
+    private int appointmentId;
+    private Patient patient;
+    private Doctor doctor;
+    private String date;
+    private String time;
 
-public Appointment(int appointmentId, Patient patient, Doctor doctor, String date, String time) {
-    this.appointmentId = appointmentId;
-    this.patient = patient;
-    this.doctor = doctor;
-    this.date = date;
-    this.time = time;
-}
-
-public void setAppointmentId(int appointmentId) {
-    this.appointmentId = appointmentId;
+    public Appointment(int appointmentId, Patient patient, Doctor doctor, String date, String time) {
+        this.appointmentId = appointmentId;
+        this.patient = patient;
+        this.doctor = doctor;
+        this.date = date;
+        this.time = time;
     }
+
+    public void setAppointmentId(int appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
     public int getAppointmentId() {
-    return appointmentId;
+        return appointmentId;
     }
 
-public void setPatient(Patient patient) {
-    this.patient = patient;
+    public void setPatient(Patient patient) {
+        this.patient = patient;
     }
+
     public Patient getPatient() {
-    return patient;
+        return patient;
     }
 
-public void setDoctor(Doctor doctor) {
-    this.doctor = doctor;
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
     }
+
     public Doctor getDoctor() {
-    return doctor;
+        return doctor;
     }
 
-public void setDate(String date) {
-    this.date = date;
+    public void setDate(String date) {
+        this.date = date;
     }
+
     public String getDate() {
-    return date;
+        return date;
     }
 
-public void setTime(String time) {
-    this.time = time;
+    public void setTime(String time) {
+        this.time = time;
     }
+
     public String getTime() {
-    return time;
+        return time;
     }
 
-public void displayInfo() {
-    System.out.println("Appointment ID: " + appointmentId);
-    System.out.println("Patient: " + patient.getName());
-    System.out.println("Doctor: " + doctor.getName());
-    System.out.println("Date: " + date);
-    System.out.println("Time: " + time);
+    public void displayInfo() {
+        System.out.println("Appointment ID: " + appointmentId);
+        System.out.println("Patient: " + patient.getName());
+        System.out.println("Doctor: " + doctor.getName());
+        System.out.println("Date: " + date);
+        System.out.println("Time: " + time);
 
-   }
+    }
 
 }
